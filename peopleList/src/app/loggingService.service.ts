e0748fd55b5aa7de629a8fb9e0453baab7f0fe7d
Sol <solzito.1889@gmail.com>
2024-09-19 19:05:22 -0300

    injectable de service

--- peopleList/src/app/loggingService.service.ts
+++ peopleList/src/app/loggingService.service.ts
@@ -1,3 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
 export class LoggingService {
   sendMessage(message: string) {
     console.log(message);
--- peopleList/src/app/personaService.service.ts
+++ peopleList/src/app/personaService.service.ts
@@ -1,3 +1,4 @@
+import { LoggingService } from './loggingService.service';
 import { Person } from './person/person.module';
 import { Injectable } from '@angular/core';
 
@@ -11,7 +12,10 @@ export class PeopleService {
     new Person('Karla', 'Lara', 'Martillero'),
   ];
 
+  constructor(private message: LoggingService) {}
+
   addPersonToList(person: Person) {
+    this.message.sendMessage('Se cargo persona: ' + person.name);
     this.people.push(person);
   }
 }
